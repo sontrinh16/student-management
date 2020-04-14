fa1f9526596cbf0573f9fb1f7e7cbc61cfc152d0
sontrinh <trinhleson2000@gmail.com>
2020-04-14 14:48:58 +0700

    fix database connection

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: "localhost",
+    host: process.env.SQL_HOST,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASS,
     database: process.env.SQL_DATABASE
