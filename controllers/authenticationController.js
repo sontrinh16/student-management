745a66904af984b110f298660190fd534859fd2e
sontrinh <trinhleson2000@gmail.com>
2020-06-12 18:09:17 +0700

    fixing password update issues

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -43,6 +43,7 @@ exports.login =catchAsync( async (req, res, next) => {
             Date.now() + 24 * 60 *60 *1000
         ),
         //httpOnly: true,
+        sameSite: 'None',
         secure: true
     }
 
